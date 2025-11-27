2ea2d0c45b63f398dcc1c0b5f8652cdb279ac980
vaishnavipawar2854 <vaishnavipawar285042gmail.com>
2025-11-27 12:07:59 +0530

    api configuration

--- frontend/realestate-ui/src/api.js
+++ frontend/realestate-ui/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // Use Vite environment variable for backend base URL if provided
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = "https://real-state-1-80ov.onrender.com/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
