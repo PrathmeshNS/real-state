b179c1d74ac07828b590d07d558929d31795ae9d
vaishnavipawar2854 <vaishnavipawar285042gmail.com>
2025-11-27 11:23:21 +0530

    api configuration into the frontend!!

--- frontend/realestate-ui/src/api.js
+++ frontend/realestate-ui/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api", // or http://localhost:8000/api
+  baseURL: "https://real-state-1-80ov.onrender.com/api", // or http://localhost:8000/api
 });
 
 export default api;
